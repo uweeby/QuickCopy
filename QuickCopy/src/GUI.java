036ad872b70a440987cb69f05fe5e1f05bf9cdee
Barry Brooks <uweenukr@gmail.com>
2012-05-22 22:04:43 -0400

    Cleaning up the GUI code a little more

--- QuickCopy/src/GUI.java
+++ QuickCopy/src/GUI.java
@@ -1,15 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
 
-
 public class GUI extends JFrame {
 	
-	private JTextField textfield1;
-	private JTextField textfield2;
-	private JTextField textfield3;
-	private JTextField textfield4;
-	private JTextField textfield5;
-	private JTextField textfield6;
+	private final JTextField textfield1;
+	private final JTextField textfield2;
+	private final JTextField textfield3;
+	private final JTextField textfield4;
+	private final JTextField textfield5;
+	private final JTextField textfield6;
 	
 	private JButton copybutton1;
 	private JButton copybutton2;
@@ -17,7 +16,6 @@ public class GUI extends JFrame {
 	private JButton copybutton4;
 	private JButton copybutton5;
 	private JButton copybutton6;
-	
 	private JButton savebutton;
 	
 	public GUI() {
@@ -27,76 +25,46 @@ public class GUI extends JFrame {
         Load load = new Load(QuickCopy.filename);
         load.LoadMethod();
 		
+        //Setup textfield and copy button
 		textfield1 = new JTextField(load.getField(1), 35);
-		add(textfield1);
 		copybutton1 = new JButton("Copy");
-		copybutton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	//Create clipboard object and paste the value of the TextField
-        		ToClipboard toclipboard = new ToClipboard();
-        		toclipboard.copyfrom(textfield1.getText());
-            }
-        });
+		event(copybutton1, textfield1);
+		add(textfield1);
 		add(copybutton1);
 		
+		//Setup textfield and copy button
 		textfield2 = new JTextField(load.getField(2), 35);
-		add(textfield2);
 		copybutton2 = new JButton("Copy");
-		copybutton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	//Create clipboard object and paste the value of the TextField
-        		ToClipboard toclipboard = new ToClipboard();
-        		toclipboard.copyfrom(textfield2.getText());
-            }
-        });
+		event(copybutton2, textfield2);
+		add(textfield2);
 		add(copybutton2);
-	
+		
+		//Setup textfield and copy button
 		textfield3 = new JTextField(load.getField(3), 35);
-		add(textfield3);
 		copybutton3 = new JButton("Copy");
-		copybutton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	//Create clipboard object and paste the value of the TextField
-        		ToClipboard toclipboard = new ToClipboard();
-        		toclipboard.copyfrom(textfield3.getText());
-            }
-        });
+		event(copybutton3, textfield3);
+		add(textfield3);
 		add(copybutton3);
 		
+		//Setup textfield and copy button
 		textfield4 = new JTextField(load.getField(4), 35);
-		add(textfield4);
 		copybutton4 = new JButton("Copy");
-		copybutton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	//Create clipboard object and paste the value of the TextField
-        		ToClipboard toclipboard = new ToClipboard();
-        		toclipboard.copyfrom(textfield4.getText());
-            }
-        });
+		event(copybutton4, textfield4);
+		add(textfield4);
 		add(copybutton4);
 		
+		//Setup textfield and copy button
 		textfield5 = new JTextField(load.getField(5), 35);
-		add(textfield5);
 		copybutton5 = new JButton("Copy");
-		copybutton5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	//Create clipboard object and paste the value of the TextField
-        		ToClipboard toclipboard = new ToClipboard();
-        		toclipboard.copyfrom(textfield5.getText());
-            }
-        });
+		event(copybutton5, textfield5);
+		add(textfield5);
 		add(copybutton5);
 		
+		//Setup textfield and copy button
 		textfield6 = new JTextField(load.getField(6), 35);
-		add(textfield6);
 		copybutton6 = new JButton("Copy");
-		copybutton6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-            	//Create clipboard object and paste the value of the TextField
-        		ToClipboard toclipboard = new ToClipboard();
-        		toclipboard.copyfrom(textfield6.getText());
-            }
-        });
+		event(copybutton6, textfield6);
+		add(textfield6);
 		add(copybutton6);
 		
 		savebutton = new JButton("Save");
@@ -123,10 +91,20 @@ public class GUI extends JFrame {
         Load load = new Load(QuickCopy.filename);
         load.LoadMethod();
         textfield1.setText(load.getField(1));
-        textfield1.setText(load.getField(2));
-        textfield1.setText(load.getField(3));
-        textfield1.setText(load.getField(4));
-        textfield1.setText(load.getField(5));
-        textfield1.setText(load.getField(6));
+        textfield2.setText(load.getField(2));
+        textfield3.setText(load.getField(3));
+        textfield4.setText(load.getField(4));
+        textfield5.setText(load.getField(5));
+        textfield6.setText(load.getField(6));
+	}
+	
+	public void event(JButton button, final JTextField field) {
+		button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            	//Create clipboard object and paste the value of the TextField
+        		ToClipboard toclipboard = new ToClipboard();
+        		toclipboard.copyfrom(field.getText());
+            }
+        });
 	}
 }
